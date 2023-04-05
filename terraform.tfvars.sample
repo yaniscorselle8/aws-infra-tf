572f15a4b1e7a430074e5826c8314327f1fcf6b4
Yanis Corselle <yanis.corselle@laposte.net>
2023-04-05 09:45:03 +0200

    initializing tf project with required providers + its associated variables (using a terraform.tfvars file to stock aws access key values to keep them outside git

---
+++ terraform.tfvars.sample
@@ -0,0 +1,2 @@
+secret_key = "your-secret-key"
+access_key = "your-access-key"
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable region {
+    default = "us-east-1"
+}
+
+variable access_key {} #to set in terraform.tfvars
+
+variable secret_key {} #to set in terraform.tfvars
